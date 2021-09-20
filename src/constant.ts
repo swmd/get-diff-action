e845393eb1e4a29c961e72ea025536ca59d62af8
swmd <frontdev8@gmail.com>
2021-09-20 16:24:40 -0400

    add target event

--- src/constant.ts
+++ src/constant.ts
@@ -6,6 +6,13 @@ export const TARGET_EVENTS = {
     'closed',
     'ready_for_review',
   ],
+  'pull_request_target': [
+    'opened',
+    'reopened',
+    'synchronize',
+    'closed',
+    'ready_for_review',
+  ],
   'push': '*',
 };
 export const REMOTE_NAME   = 'get-diff-action';
